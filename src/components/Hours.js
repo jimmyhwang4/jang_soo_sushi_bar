45a4cccff946c4b8d3e4ab91e39b4577a84919cc
jimmyhwang4 <35846105+jimmyhwang4@users.noreply.github.com>
2020-06-14 23:06:28 -0700

    Adjusted hours

--- src/components/Hours.js
+++ src/components/Hours.js
@@ -8,31 +8,31 @@ function Hours() {
             </div>
             <div className="hoursDay">
                 <p>MON</p>
-                <p>CLOSED</p>
+                <p>11:30AM - 9:30PM</p>
             </div>
             <div className="hoursDay">
                 <p>TUE</p>
-                <p>11:30AM - 8PM</p>
+                <p>11:30AM - 9:30PM</p>
             </div>
             <div className="hoursDay">
                 <p>WED</p>
-                <p>11:30AM - 8PM</p>
+                <p>11:30AM - 9:30PM</p>
             </div>
             <div className="hoursDay">
                 <p>THU</p>
-                <p>11:30AM - 8PM</p>
+                <p>11:30AM - 9:30PM</p>
             </div>
             <div className="hoursDay">
                 <p>FRI</p>
-                <p>11:30AM - 8PM</p>
+                <p>11:30AM - 9:30PM</p>
             </div>
             <div className="hoursDay">
                 <p>SAT</p>
-                <p>11:30AM - 8PM</p>
+                <p>11:30AM - 9:30PM</p>
             </div>
             <div className="hoursDay">
                 <p>SUN</p>
-                <p>12PM - 8PM</p>
+                <p>12PM - 9PM</p>
             </div>
         </div>
     );
