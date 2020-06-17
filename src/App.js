0efc5df3c67c3571c709f3917ae2adbd23b767b7
jimmyhwang4 <35846105+jimmyhwang4@users.noreply.github.com>
2020-06-16 17:55:14 -0700

    Fixed bugs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, BrowserRouter as Router, Redirect } from "react-router-dom";
+import { Route, BrowserRouter as Router} from "react-router-dom";
 import './App.css';
 
 import Home from "./pages/Home";
--- src/components/Location.js
+++ src/components/Location.js
@@ -7,8 +7,8 @@ function Location() {
                 <h3>LOCATION</h3>
             </div>
             <div className="locationImage">
-                <a href="https://www.google.com/maps/place/Jang+Soo+Sushi+Bar+Restaurant/@33.8336871,-118.0705251,14.72z/data=!4m5!3m4!1s0x0:0x5414563e9a071725!8m2!3d33.8319322!4d-118.0636317" target="_blank">
-                    <img src="location.jpg" width="100%" height="auto"></img>
+                <a href="https://www.google.com/maps/place/Jang+Soo+Sushi+Bar+Restaurant/@33.8336871,-118.0705251,14.72z/data=!4m5!3m4!1s0x0:0x5414563e9a071725!8m2!3d33.8319322!4d-118.0636317" target="_blank" rel="noopener noreferrer">
+                    <img alt="google_map" src="location.jpg" width="100%" height="auto"></img>
                 </a>
             </div>
             <div className="locationText">
