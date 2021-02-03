00e21195106b09627428afe3f64f72c630adc738
jimmy <jh5397@nyu.edu>
2021-02-03 02:15:46 -0800

    COVID update

--- src/pages/COVID.js
+++ src/pages/COVID.js
@@ -15,10 +15,12 @@ function COVID() {
                         <p>We are fully aware of the impact of COVID-19 in our community, and during these challenging times, we are prioritizing the health and safety of you and our family of employees. Currently, we are following the guidance and recommendations from the City of Hawaiian Gardens and the Centers for Disease Control and Prevention (CDC). We have implemented necessary changes in order to maintain the highest possible standards in service and quality.</p>
                         <p><br></br></p>
                         <p>These changes include:</p>
-                        <p>• Only providing take-outs while strictly limiting physical interaction between customers and staff.</p>
+                        <p>• Only providing outdoor dining and take-outs while strictly limiting physical interaction between customers and staff.</p>
                         <p>• Increasing the frequency of cleaning practices and decontamination procedures within our restaurant.</p>
                         <p>• Increased presence and use of hand sanitizers and soap.</p>
                         <p>• Ensuring that our staff is wearing masks and educating them on preventative measures and new updates.</p>
+                        <p><br></br></p>
+                        <p>Regarding outdoor dining, new COVID-19 regulations only allow groups from the same household, up to 6 people per table. Our tables will be separated 8 feet away from each other, and all customers are required to wear a mask upon entry. We will also be required to take down the phone number of one person. Please call us at (562) 924-2498 if you would like to make a reservation.</p>
                         <p><br></br></p>
                         <p>As for the possibility of a full reopening, we are actively monitoring current events and still gauging the appropriate time for this. An update will be posted in the near future.</p>
                         <p><br></br></p>
