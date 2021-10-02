5a904c7c1a78c08329c84367ca343fbbd898fc41
jimmy <jh5397@nyu.edu>
2021-10-01 21:44:13 -0400

    Hours update

--- src/components/Hours.js
+++ src/components/Hours.js
@@ -8,31 +8,31 @@ function Hours() {
             </div>
             <div className="hoursDay">
                 <p>MON</p>
-                <p>11:30AM - 3PM, 4PM - 9:30PM</p>
+                <p>11:30AM - 3PM, 4:30PM - 9PM</p>
             </div>
             <div className="hoursDay">
                 <p>TUE</p>
-                <p>11:30AM - 3PM, 4PM - 9:30PM</p>
+                <p>11:30AM - 3PM, 4:30PM - 9PM</p>
             </div>
             <div className="hoursDay">
                 <p>WED</p>
-                <p>11:30AM - 3PM, 4PM - 9:30PM</p>
+                <p>11:30AM - 3PM, 4:30PM - 9PM</p>
             </div>
             <div className="hoursDay">
                 <p>THU</p>
-                <p>11:30AM - 3PM, 4PM - 9:30PM</p>
+                <p>11:30AM - 3PM, 4:30PM - 9PM</p>
             </div>
             <div className="hoursDay">
                 <p>FRI</p>
-                <p>11:30AM - 3PM, 4PM - 9:30PM</p>
+                <p>11:30AM - 3PM, 4:30PM - 9PM</p>
             </div>
             <div className="hoursDay">
                 <p>SAT</p>
-                <p>11:30AM - 9:30PM</p>
+                <p>11:30AM - 3PM, 4:30PM - 9PM</p>
             </div>
             <div className="hoursDay">
                 <p>SUN</p>
-                <p>12:00PM - 9:00PM</p>
+                <p>12PM - 3PM, 4:30PM - 9PM</p>
             </div>
         </div>
     );
